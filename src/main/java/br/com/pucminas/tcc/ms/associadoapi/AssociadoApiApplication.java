bcfab86d0e3c0c3514f00867fc11d822af08fd32
NELSON Pereira de Castro <nelsoncastro.eireli@poupex.com.br>
2022-02-02 19:52:15 -0300

    feat: Adicionar configuração de segurança

--- src/main/java/br/com/pucminas/tcc/ms/associadoapi/AssociadoApiApplication.java
+++ src/main/java/br/com/pucminas/tcc/ms/associadoapi/AssociadoApiApplication.java
@@ -1,13 +1,20 @@
 package br.com.pucminas.tcc.ms.associadoapi;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AssociadoApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AssociadoApiApplication.class, args);
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
     }
 
 }
