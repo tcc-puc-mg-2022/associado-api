8aa83a547add11ee2edadd02fe6c8f2fe31d2524
NELSON Pereira de Castro <nelsoncastro.eireli@poupex.com.br>
2022-02-05 11:06:06 -0300

    feat: adicionar docker file

--- src/main/java/br/com/pucminas/tcc/ms/associadoapi/rabbitmq/configuration/QueueConfig.java
+++ src/main/java/br/com/pucminas/tcc/ms/associadoapi/rabbitmq/configuration/QueueConfig.java
@@ -2,6 +2,7 @@ package br.com.pucminas.tcc.ms.associadoapi.rabbitmq.configuration;
 
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
@@ -10,11 +11,20 @@ import java.util.concurrent.TimeoutException;
 @Configuration
 public class QueueConfig {
 
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitmqHost;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitmqUser;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitmqPassword;
+
     public Connection getConnection() throws IOException, TimeoutException {
         final var factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setUsername("admin");
-        factory.setPassword("admin");
+        factory.setHost(rabbitmqHost);
+        factory.setUsername(rabbitmqUser);
+        factory.setPassword(rabbitmqPassword);
 
         return factory.newConnection();
     }
